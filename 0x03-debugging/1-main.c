d42c38c88aba0076b623de60fa3d94405ea51fbf
Manyouu <pharaohmanyou00yahoo.com>
2022-12-16 05:52:21 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,18 +9,16 @@ int main(void)
 {
 int i;
 
-
 printf("infinite loop incoming :(\n");
 
 i = 0;
 /*
- * while (i < 10)
- * {
- *      putchar(i);
- * }
+ *while (i < 10)
+ *{
+ *putchar(i);
+ *}
  */
 print("infinite loop avoided! \\o/\n");
-
 
 return (0);
 }
