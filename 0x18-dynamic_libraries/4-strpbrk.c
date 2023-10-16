fc24c9c211b5d85716c2e55151e782dc3119579f
manyouu <pharaohmanyou00@yahoo.com>
2023-10-16 15:48:17 -0700

    all task

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -1,23 +1,29 @@
 #include "main.h"
 /**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
- */
+*_strpbrk - The _strpbrk() function locates the first
+* occurrence in the string s of any of the bytes in
+*the string accept
+*
+*@s: string where search is made
+*@accept: string where searched bytes are located
+*
+*Return:Returns a pointer to the byte in s that matches
+* one of the bytes in accept, or NULL if no such byte is found
+*/
+
 char *_strpbrk(char *s, char *accept)
 {
-		int k;
+	int i;
 
-		while (*s)
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
 		{
-			for (k = 0; accept[k]; k++)
-			{
-			if (*s == accept[k])
-			return (s);
-			}
-		s++;
+			if (accept[i] == *s)
+				return (s);
 		}
+		s++;
+	}
 
 	return ('\0');
 }
